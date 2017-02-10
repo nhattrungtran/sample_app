7e148d5ff06464327c5157cf9689a7949c9b19b8
nhattrung <trungtranqn91@gmail.com>
2017-02-11 06:43:16 +0700

    Make a basic User model (including secure passwords)

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,17 @@
+class StaticPagesController < ApplicationController
+   protect_from_forgery with: :exception
+  def home
+  	
+  end
+
+  def help
+  
+  end
+
+  def about
+  end
+  def contact
+  end	
+
+
+end
