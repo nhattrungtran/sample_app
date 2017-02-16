e79560eeacd7a7503efe74d1838e469221de23fe
nhattrung <trungtranqn91@gmail.com>
2017-02-16 12:50:52 +0700

    changed file user.irb=rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -41,9 +41,8 @@ class User < ApplicationRecord
       end
       # Activates an account.
       def activate
-        update_columns(activated: true, activated_at: Time.zone.now)
-        # update_attribute(:activated,    true)
-        # update_attribute(:activated_at, Time.zone.now)
+        update_attribute(:activated,    true)
+        update_attribute(:activated_at, Time.zone.now)
       end
 
       # Sends activation email.
